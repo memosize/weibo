d9e638924f0f1efbe4305c0fde2c373f6806468d
memosize <89068024@qq.com>
2017-03-31 13:20:09 +0800

    初始化首页

---
+++ weibp/weibp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibp
+//
+//  Created by 杨森林 on 17/3/31.
+//  Copyright © 2017年 dasousuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibp/weibp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibp
+//
+//  Created by 杨森林 on 17/3/31.
+//  Copyright © 2017年 dasousuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
